b7168cdd31163c58afb53ff1aa44861a813b16c3
Samantha Felder <LE1937911@crc-lennox.qc.ca>
2021-02-17 14:37:46 -0500

    test commit

---
+++ balancer.js
@@ -0,0 +1,27 @@
+var http = require('http');
+
+http.createServer(function (req, res) {
+  var http = require('http');
+
+
+var callingOtherServerRequest = http.request("http://localhost:1111", function(response) {
+    var str = '';
+    //another chunk of data has been received, so append it to `str`
+    response.on('data', function (chunk) {
+      str += chunk;
+    });
+  
+    //the whole response has been received, so we just print it out here
+    response.on('end', function () {
+      console.log(str);
+    });
+
+  }).end();
+
+  callingOtherServerRequest.on('error', function (e) {
+    console.info("server is not responding");
+  });
+  
+  res.end();
+}).listen(9999);
+console.info("Server running ....");
