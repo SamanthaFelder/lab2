441d4bd94679493e657d8895867694a4040fbb85
Samantha Felder <LE1937911@crc-lennox.qc.ca>
2021-03-01 18:23:43 -0500

    Final

--- balancer.js
+++ balancer.js
@@ -1,27 +1,41 @@
 var http = require('http');
+var serverArray = ["http://localhost:1111", "http://localhost:2222", "http://localhost:3333"];
+var indexServer = 0;
 
-http.createServer(function (req, res) {
-  var http = require('http');
+function callServer(req, res) {
+  if(req.url == "/hello"){
+    http.get(serverArray[indexServer], function(response) {
+        var str = '';
+        console.log(indexServer);
+        //another chunk of data has been received, so append it to `str`
+        response.on('data', function(chunk) {
+            str += chunk;
+        });
 
+        //the whole response has been received, so we just print it out here
+        response.on('end', function() {
+            console.log(str);
+            res.writeHead(200, {
+              'Content-Type': 'text/html'
+          });
+          res.end(str);
+        });
 
-var callingOtherServerRequest = http.request("http://localhost:1111", function(response) {
-    var str = '';
-    //another chunk of data has been received, so append it to `str`
-    response.on('data', function (chunk) {
-      str += chunk;
+        if (indexServer == 2) {
+            indexServer = 0;
+        } else {
+            indexServer += 1;
+        }
+    }).on('error', function(e) {
+        console.info("server is not responding");
+        if (indexServer == 2) {
+            indexServer = 0;
+        } else {
+            indexServer += 1;
+        }
+        callServer(req, res);
     });
-  
-    //the whole response has been received, so we just print it out here
-    response.on('end', function () {
-      console.log(str);
-    });
-
-  }).end();
-
-  callingOtherServerRequest.on('error', function (e) {
-    console.info("server is not responding");
-  });
-  
-  res.end();
-}).listen(9999);
-console.info("Server running ....");
+}
+}
+http.createServer(callServer).listen(9999);
+console.info["Server running ...."];
